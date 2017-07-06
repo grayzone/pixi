2d88889072167d4df28dbf31721ede41be4bb12d
grayzone <grayzone137@gmail.com>
2017-07-06 14:19:06 +0800

    add key board

--- jsx/lib/template.jsx
+++ jsx/lib/template.jsx
@@ -43,17 +43,112 @@ export default class Template extends React.Component {
     //    sprite.width = window.innerWidth;
     //    sprite.height = window.innerWidth;
     this.sprite.scale.set(0.5, 0.5);
+    this.sprite.vx = 0;
+    this.sprite.vy = 0;
     this.stage = new Container();
     this.stage.addChild(this.sprite);
+    this.imageState();
     this.moveLoop();
     //this.renderer.render(this.stage);
     console.log("image is loaded");
   };
 
+  imageState = () => {
+    this.play();
+  };
+
+  play = () => {
+    this.sprite.x += this.sprite.vx;
+    this.sprite.y += this.sprite.vy;
+  };
+
   moveLoop = () => {
     requestAnimationFrame(this.moveLoop);
-    this.sprite.x += 1;
+    /*this.sprite.vx = 1;
+    this.sprite.vy = 1;
+
+    this.sprite.x += this.sprite.vx;
+    this.sprite.y += this.sprite.vy;
+*/
+    this.keyControl();
+    this.imageState();
     this.renderer.render(this.stage);
+  };
+
+  keyControl = () => {
+    var left = this.keyboard(37);
+    var up = this.keyboard(38);
+    var right = this.keyboard(39);
+    var down = this.keyboard(40);
+    left.press = () => {
+      this.sprite.vx = -5;
+      this.sprite.vy = 0;
+    };
+    left.release = () => {
+      if (!right.isDown && this.sprite.vy === 0) {
+        this.sprite.vx = 0;
+      }
+    };
+    up.press = () => {
+      this.sprite.vx = 0;
+      this.sprite.vy = -5;
+    };
+    up.release = () => {
+      if (!down.isDown && this.sprite.vx === 0) {
+        this.sprite.vy = 0;
+      }
+    };
+    right.press = () => {
+      this.sprite.vx = 5;
+      this.sprite.vy = 0;
+    };
+    right.release = () => {
+      if (!left.isDown && this.sprite.vy === 0) {
+        this.sprite.vx = 0;
+      }
+    };
+    down.press = () => {
+      this.sprite.vx = 0;
+      this.sprite.vy = 5;
+    };
+    down.release = () => {
+      if (!up.isDown && this.sprite.vx === 0) {
+        this.sprite.vy = 0;
+      }
+    };
+  };
+
+  keyboard = keyCode => {
+    var key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+
+    key.downHandler = event => {
+      if (event.keyCode == key.code) {
+        if (key.isUp && key.press) {
+          key.press();
+        }
+        key.isDown = true;
+        key.isUp = false;
+      }
+      event.preventDefault();
+    };
+    key.upHandler = event => {
+      if (event.keyCode == key.code) {
+        if (key.isDown && key.release) {
+          key.release();
+        }
+        key.isDown = false;
+        key.isUp = true;
+      }
+      event.preventDefault();
+    };
+    window.addEventListener("keydown", key.downHandler.bind(key), false);
+    window.addEventListener("keyup", key.upHandler.bind(key), false);
+    return key;
   };
 
   loadProgressHandler = (loader, resource) => {
